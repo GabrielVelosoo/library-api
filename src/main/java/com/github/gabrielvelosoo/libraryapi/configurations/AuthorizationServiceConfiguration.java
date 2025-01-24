0b96ada4ef2367dc5d7212c374355713df712ee8
Gabriel Veloso <gaabrielvelooso@gmail.com>
2025-01-24 16:27:52 -0300

    Working with OAuth2

--- src/main/java/com/github/gabrielvelosoo/libraryapi/configurations/AuthorizationServiceConfiguration.java
+++ src/main/java/com/github/gabrielvelosoo/libraryapi/configurations/AuthorizationServiceConfiguration.java
@@ -1,5 +1,10 @@
 package com.github.gabrielvelosoo.libraryapi.configurations;
 
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -8,6 +13,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
@@ -15,7 +21,12 @@ import org.springframework.security.oauth2.server.authorization.settings.OAuth2T
 import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.time.Duration;
+import java.util.UUID;
 
 @Configuration
 @EnableWebSecurity
@@ -24,11 +35,10 @@ public class AuthorizationServiceConfiguration {
     @Bean
     @Order(1)
     public SecurityFilterChain authServiceSecurityFilterChain(HttpSecurity http) throws Exception {
-        OAuth2AuthorizationServerConfiguration auth2AuthorizationServerConfiguration = new OAuth2AuthorizationServerConfiguration();
-        auth2AuthorizationServerConfiguration.authorizationServerSecurityFilterChain(http);
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
         http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
                 .oidc(Customizer.withDefaults());
-        http.oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt(Customizer.withDefaults()));
+        http.oauth2ResourceServer(oauth2Rs -> oauth2Rs.jwt(Customizer.withDefaults()));
         http.formLogin(configurer -> configurer.loginPage("/login"));
         return http.build();
     }
@@ -44,6 +54,7 @@ public class AuthorizationServiceConfiguration {
                 .builder()
                 .accessTokenFormat(OAuth2TokenFormat.SELF_CONTAINED)
                 .accessTokenTimeToLive(Duration.ofMinutes(60))
+                .refreshTokenTimeToLive(Duration.ofMinutes(90))
                 .build();
     }
 
@@ -53,5 +64,30 @@ public class AuthorizationServiceConfiguration {
                 .builder()
                 .requireAuthorizationConsent(false)
                 .build();
+    }
+
+    @Bean
+    public JWKSource<SecurityContext> generateJWKToken() throws Exception {
+        RSAKey rsaKey = generateRSAKeyPair();
+        JWKSet jwkSet = new JWKSet(rsaKey);
+        return new ImmutableJWKSet<>(jwkSet);
+    }
+
+    private RSAKey generateRSAKeyPair() throws Exception {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(2048);
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+        return new RSAKey
+                .Builder(publicKey)
+                .privateKey(privateKey)
+                .keyID(UUID.randomUUID().toString())
+                .build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
+        return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
     }
 }
--- src/main/java/com/github/gabrielvelosoo/libraryapi/controllers/LoginViewController.java
+++ src/main/java/com/github/gabrielvelosoo/libraryapi/controllers/LoginViewController.java
@@ -3,6 +3,7 @@ package com.github.gabrielvelosoo.libraryapi.controllers;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -17,5 +18,11 @@ public class LoginViewController {
     @ResponseBody
     public String homePage(Authentication authentication) {
         return "Olá, " + authentication.getName();
+    }
+
+    @GetMapping(value = "/authorized")
+    @ResponseBody
+    public String getAuthorizationCode(@RequestParam("code") String code) {
+        return "Your authorization code: " + code;
     }
 }
